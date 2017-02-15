195ac68e08f60340433226b2b3ed0d69d87d8660
Jaime Cervantes Velasco <jaime.cervantes.ve@gmail.com>
2017-02-14 19:01:22 -0600

    Adding examples of import, template and shadow dom

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var path = require('path');
 
 var app = express();
 
-app.set('port', process.env.PORT || 5000 );
+app.set('port', process.env.PORT || 3000 );
 
 app.use( express.static( path.resolve( __dirname ) ) );
 
